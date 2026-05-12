16c80be44ac1bb2ac2b99948e6d17a4d1a54d6ef
shivam <sattaapppp@gmail.com>
2026-05-12 08:30:23 +0530

    Add Vercel Analytics, remove old API serverless function, clean up vercel.json

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { Analytics } from "@vercel/analytics/react";
 import App from './App';
 import './index.css';
 import './i18n'; // Initialize i18n
@@ -8,5 +9,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>
 );
